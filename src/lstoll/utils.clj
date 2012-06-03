2db8a3d79ab03effa7bf0d734272fa57f1ede7a6
Lincoln Stoll <lstoll@lstoll.net>
2012-06-03 17:43:53 +1000

    Add a docstring

--- src/lstoll/utils.clj
+++ src/lstoll/utils.clj
@@ -10,6 +10,7 @@
        (catch Exception e {}))) ; No env if no file.
 
 (defn env [k & [v]]
+  "This will retrieve a value from the current environment. If it's not there, it will fall back in to looking for the value in a .env file in the project root (in 'foreman' key=value format). If the value doesn't exist there, it will fall back to either the default passed in, or nil"
   (or (System/getenv k)
       (get (dotenv) k)
       v))
