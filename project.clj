a3eb443a86337c4a88c0104692e1f658756f3824
Lincoln Stoll <lstoll@me.com>
2012-10-19 16:50:08 +0200

    v0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.lstoll/utils "0.3.1"
+(defproject net.lstoll/utils "0.3.2"
   :description "A set of common utility functions used by lstoll"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
